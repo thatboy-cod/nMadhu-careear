5cdecad7b9605f0d976aa1c66b758bcd4e56314a
Madhu sudan pandia <116194615+thatboy-cod@users.noreply.github.com>
2024-04-13 18:49:34 +0000

    added img and property

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-from flask import Flask
-
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 
 def hello_world():
-  return "hellow nabu"
+  return render_template('home.html')
 if __name__ == "__main__":
-  print("iam inside the if now ")
   app.run(host="0.0.0.0",debug=True)
