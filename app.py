e0a4b89ce16294f2a6b86e0fcf65fdf978c8c623
Madhu sudan pandia <116194615+thatboy-cod@users.noreply.github.com>
2024-04-30 05:08:18 +0000

    added dynamic data and  api route

--- app.py
+++ app.py
@@ -1,23 +1,33 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,jsonify
 
 app = Flask(__name__)
-@app.route("/")
-# jobs =[{'id':1,'title':'Data Analyst',
-#        'location':'Bengaluru,Indaia',
-#        'Salary':'Rs. 10,00,000'},{'id':2,'title':'Data SCientist',
-#          'location':'Delhi,Indaia',
-#          'Salary':'Rs 15,00,000'},{'id':3,'title':'FrontenEnd Engineer','location':'Remote','Salary':'Rs. 12,00,000'},{'id':4,'title':'Backend Engineer',
-#          'location':'san-francisco,Usa',
-#          'Salary':'$15,00,00'}]
-
+JOBS =[
+  {     'id':1,
+        'title':'Data Analyst',
+        'location':'Bengaluru,Indaia',
+       ' Salary':'Rs. 10,00,000'
+  },
+  {'id':2,
+       'title':'Data SCientist',
+      'location':'Delhi,Indaia',
+      'Salary':'Rs 15,00,000'
+  },
+  {'id':3,
+       'title':'FrontenEnd Engineer',
+       'location':'Remote',
+       'Salary':'Rs. 12,00,000'
+  },
+  {'id':4,
+       'title':'Backend Engineer',
+      'location':'san-francisco,Usa',
+      'Salary':'$15,00,00'
+  }]
+@app.route("/")  
 def hello_world():
-  jobs =[{'id':1,'title':'Data Analyst',
-     'location':'Bengaluru,Indaia',
-     'Salary':'Rs. 10,00,000'},{'id':2,'title':'Data SCientist',
-       'location':'Delhi,Indaia',
-       'Salary':'Rs 15,00,000'},{'id':3,'title':'FrontenEnd Engineer','location':'Remote','Salary':'Rs. 12,00,000'},{'id':4,'title':'Backend Engineer',
-       'location':'san-francisco,Usa',
-       'Salary':'$15,00,00'}]
-  return render_template('home.html',jobs=jobs,company_name="Thatboys")
+  return render_template('home.html',jobs=JOBS,company_name="Thatboys")
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 if __name__ == "__main__":
   app.run(host="0.0.0.0",debug=True)
